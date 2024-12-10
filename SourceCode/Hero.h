748f21305770c8594c7a707a1cd446908d4378a4
quantankevin <kevin980034@gmail.com>
2024-12-10 13:57:57 +0800

    first commit

---
+++ SourceCode/Hero.h
@@ -0,0 +1,28 @@
+#ifndef HERO_H_INCLUDED
+#define HERO_H_INCLUDED
+
+#include "Object.h"
+#include <map>
+#include <string>
+enum class HeroState{
+    LEFT, 
+    RIGHT, 
+    FRONT, 
+    BACK,
+    HEROSTATE_MAX
+};
+
+class Hero : public Object
+{
+    public:
+        void init();
+        void update();
+        void draw();
+    private:
+        HeroState state = HeroState::FRONT;
+        double speed = 5;
+        std::map<HeroState, std::string> gif_path;
+};
+
+
+#endif // HERO_H_INCLUDED
